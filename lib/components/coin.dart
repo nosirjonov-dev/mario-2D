123a425ce6b52894d35694d8bdc6c498a5ad88df
Nosirjonov Qosimjon <nosirjonov.dev@gmail.com>
2026-02-24 12:52:31 +0500

    sprite

--- lib/components/coin.dart
+++ lib/components/coin.dart
@@ -2,11 +2,17 @@ import 'package:flame/components.dart';
 import 'package:flame/collisions.dart';
 
 class Coin extends SpriteComponent with CollisionCallbacks {
+
   Coin({required Vector2 position})
-      : super(position: position, size: Vector2(30, 30), anchor: Anchor.center);
+      : super(
+    position: position,
+    size: Vector2(30, 30),
+    anchor: Anchor.center,
+  );
 
   @override
   Future<void> onLoad() async {
+    sprite = await Sprite.load('coin.png'); // 🔥 MUHIM
     add(RectangleHitbox());
   }
 }
--- lib/components/enemy.dart
+++ lib/components/enemy.dart
@@ -2,6 +2,7 @@ import 'package:flame/components.dart';
 import 'package:flame/collisions.dart';
 
 class Enemy extends SpriteComponent with CollisionCallbacks {
+
   Vector2 startPosition;
   Vector2 endPosition;
   bool movingRight = true;
@@ -9,24 +10,34 @@ class Enemy extends SpriteComponent with CollisionCallbacks {
   Enemy({required Vector2 position})
       : startPosition = position.clone(),
         endPosition = Vector2(position.x + 100, position.y),
-        super(position: position, size: Vector2(50, 50), anchor: Anchor.center);
+        super(
+        position: position,
+        size: Vector2(50, 50),
+        anchor: Anchor.center,
+      );
 
   @override
   Future<void> onLoad() async {
+    sprite = await Sprite.load('enemy.png'); // 🔥 MUHIM
     add(RectangleHitbox());
   }
 
   @override
   void update(double dt) {
     super.update(dt);
+
     double speed = 100 * dt;
 
     if (movingRight) {
       position.x += speed;
-      if (position.x >= endPosition.x) movingRight = false;
+      if (position.x >= endPosition.x) {
+        movingRight = false;
+      }
     } else {
       position.x -= speed;
-      if (position.x <= startPosition.x) movingRight = true;
+      if (position.x <= startPosition.x) {
+        movingRight = true;
+      }
     }
   }
 }
